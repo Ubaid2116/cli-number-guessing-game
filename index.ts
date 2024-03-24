11c30372717eba8beda55685cde86b7064bf559e
Ubaid2116 <ukubaid74@gmail.com>
2024-03-24 21:10:55 +0500

    Addding a number guessing game

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("\n\tWelcome to Muhammad-Ubaid - CLI Number Guessing Gmae\n");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Enter your guess number(Number Limit from 1 to 5):",
+  },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("Congratulation ! You guess a correct number.");
+} else {
+  console.log("Sorry, You guess a wrong number.");
+}
